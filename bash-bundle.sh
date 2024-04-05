2e816c7d695fef2cd228a297343ec7e456fc38fa
Sitthykun LY <ly.sitthykun@gmail.com>
2024-04-05 23:30:02 +0700

    Create bash-bundle.sh

---
+++ bash-bundle.sh
@@ -0,0 +1,257 @@
+#!/bin/bash
+# author: masakokh
+# year: 2024
+# note: build client bundle
+# version: 1.4.7
+# argument command
+CMD_KEY=''
+CMD_VALUE=''
+# command list
+CMD_LIST_ENV='env'
+CMD_LIST_HELP='help'
+CMD_LIST_INIT='init'
+CMD_LIST_UPDATE='update'
+# environment
+ENV_PRO='pro'
+
+# declare variables
+RootPath=./assets/
+ModulePath="$RootPath"modules
+# name
+AppCss="$RootPath"app.css
+AppCssBase="$RootPath"app.base.css
+AppJs="$RootPath"app.js
+AppJsBase="$RootPath"app.base.js
+# dir
+AppJsDirPatterns=($ModulePath/[0-9]_*/js/[0-9]_*.js $ModulePath/[0-9]_*/js/[0-9][0-9]_*.js $ModulePath/[0-9][0-9]_*/js/[0-9]_*.js $ModulePath/[0-9][0-9]_*/js/[0-9][0-9]_*.js)
+AppCssDirPatterns=($ModulePath/[0-9]_*/css/[0-9]_*.css $ModulePath/[0-9]_*/css/[0-9][0-9]_*.css $ModulePath/[0-9][0-9]_*/css/[0-9]_*.css $ModulePath/[0-9][0-9]_*/css/[0-9][0-9]_*.css)
+
+# production environment
+isPro=0
+
+# cmd
+doCmd()
+{
+	# find any condition
+	for opt in "$@" ; do
+		# get command
+		CMD_KEY="${opt%=*}"
+		CMD_VALUE="${opt#*=}"
+
+		# filter cmd
+		if [[ ${CMD_KEY} = ${CMD_LIST_INIT} ]] ; then
+			# call initilize build-bundle
+			doInit
+			# exit code
+			exit 0
+		fi
+
+		# filter cmd
+		if [[ ${CMD_KEY} = ${CMD_LIST_HELP} ]] ; then
+			# call help build-bundle
+			doHelp
+			# exit code
+			exit 0
+		fi
+
+		# filter environment
+		if [[ ${CMD_KEY} = ${CMD_LIST_ENV} ]] ; then
+			# production environment
+			if [[ ${CMD_VALUE} = ${ENV_PRO} ]] ; then
+				echo "The environment is $ENV_PRO"
+				isPro=1
+				break
+			fi
+		fi
+
+		# filter update package
+		if [[ ${CMD_KEY} = ${CMD_LIST_UPDATE} ]] ; then
+			# call update build-bundle
+			doUpdate
+			# exit code
+			exit 0
+		fi
+
+	done
+}
+
+# css task
+doCss()
+{
+	# inform
+	echo -e "\e[93mCombining CSS ..."
+
+	# create app file
+	if [ ! -e $AppCss ]; then
+        touch $AppCss
+	else
+	  	# linux and mac
+	  	# truncate -s 0 $AppCss
+    	cat /dev/null > $AppCss
+	fi
+
+	# create base file
+	if [ ! -e $AppCssBase ]; then
+    	touch $AppCssBase
+  else
+		# linux and mac
+	  	# truncate -s 0 $AppJsBase
+    	cat /dev/null > $AppCssBase
+	fi
+
+	# find all in list
+	for FileCss in ${AppCssDirPatterns[@]}
+	do
+		# check file first
+		if [ -f "$FileCss" ]; then
+	    echo " > $FileCss";
+			# add new line
+			echo "" >> $AppCssBase;
+			# merge file
+			cat $FileCss >> $AppCssBase;
+		fi
+	done
+
+	# compress
+	node_modules/uglifycss/uglifycss $AppCssBase > $AppCss
+
+	# check environment
+	if [ "$isPro" -gt 0 ]; then
+		# remove base file
+		rm $AppCssBase
+	fi
+}
+
+# help
+doHelp()
+{
+  # man
+  echo "----------------------------------------------------------------------------------"
+  echo "//-- help ------------------------------------------------------------------------"
+  echo "    $ bash build-bundle.sh help"
+  echo " "
+  echo "----------------------------------------------------------------------------------"
+  echo "//-- init build-bundle env -------------------------------------------------------"
+  echo "    $ bash build-bundle.sh init"
+  echo " "
+  echo "----------------------------------------------------------------------------------"
+  echo "//-- build by environment --------------------------------------------------------"
+  echo "  The command below will run a default environment with keeping the reserve files"
+  echo "    $ bash build-bundle.sh"
+  echo "  pro is an environment that removes the reserve compression files"
+  echo "    $ bash build-bundle.sh env=pro"
+  echo "  with another environment"
+  echo "    $ bash build-bundle.sh env=dev"
+  echo " "
+  echo "----------------------------------------------------------------------------------"
+  echo "//-- update the compression libraries --------------------------------------------"
+  echo "    $ bash build-bundle.sh update"
+}
+
+# install js tools
+doInit()
+{
+	# inform
+	echo -e "\e[93mInitialize ..."
+	# update npm
+	# more: https://docs.npmjs.com/try-the-latest-stable-version-of-npm
+	#npm install -g npm@latest
+
+	echo -e "\e[93mInstalling uglifycss ..."
+	# optimize css
+	npm install uglifycss
+
+	echo -e "\e[93mInstalling terser ..."
+	# optimize js
+	npm install terser
+}
+
+# javascript task
+doJs()
+{
+	# inform
+	echo -e "\e[93mCombining JS ..."
+
+	# create app file
+	if [ ! -e $AppJsBase ]; then
+		touch $AppJsBase
+	else
+		# linux and mac
+		# truncate -s 0 $AppJsBase
+		cat /dev/null > $AppJsBase
+	fi
+
+	# create base file
+	if [ ! -e $AppJs ]; then
+		touch $AppJs
+	else
+		# linux and mac
+		# truncate -s 0 $AppJs
+    	cat /dev/null > $AppJs
+	fi
+
+	# find all in list
+	for FileJs in ${AppJsDirPatterns[@]}
+	do
+		# check file first
+		if [ -f "$FileJs" ]; then
+			echo " > $FileJs";
+			# add new line
+			echo "" >> $AppJsBase;
+			# merge file
+			cat $FileJs >> $AppJsBase;
+		fi
+	done
+
+	# compress
+	node_modules/terser/bin/terser $AppJsBase -o $AppJs
+
+	# check environment
+	if [ "$isPro" -gt 0 ]; then
+		# remove base file
+		rm $AppJsBase
+	fi
+}
+
+# inform
+doResult()
+{
+	echo -e "\e[93mFind them ..."
+	echo " > $AppCss"
+	echo " > $AppJs"
+}
+
+# update library
+doUpdate()
+{
+	# Update
+	echo -e "\e[93mUpdating ..."
+	# optimize css
+	npm update
+
+	echo -e "\e[93mFinishing ..."
+}
+
+# print out third party versions
+printVersion()
+{
+	node_modules/uglifycss/uglifycss --version
+	node_modules/terser/bin/terser --version
+}
+
+# check cmd
+doCmd $@
+
+# print out third party versions
+printVersion
+
+# start block
+echo -e "\e[92mStarting >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+# css starting
+doCss
+# js starting
+doJs
+# output path
+doResult
+# end block
+echo -e "\e[92mDone <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
